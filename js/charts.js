9432b3449e768c89ca26726b58e7cfea135e3c9b
Amanda Thomson <acfthomson@gmail.com>
2021-02-25 15:33:12 -0500

    Deliverable 2 progress

--- js/charts.js
+++ js/charts.js
@@ -102,3 +102,29 @@ function buildCharts(sample) {
 
     // 10. Use Plotly to plot the data with the layout 
     Plotly.newPlot("bar", barData, barLayout);
+
+// DELIVERABLE 2: Create a Bubble Chart
+
+    // 1. Create the trace for the bubble chart
+    var bubbleData = [{
+      x: ids,
+      y: bubbleValues,
+      text: bubbleLabels,
+      mode: "markers",
+       marker: {
+         size: bubbleValues,
+         color: bubbleValues,
+         colorscale: "Bluered" 
+       }
+    }];
+  
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+        title: "Bacteria Cultures Per Sample",
+        xaxis: {title: "OTU ID"},
+        automargin: true,
+        hovermode: "closest"
+    };
+  
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout)
